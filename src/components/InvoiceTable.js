3ecec2c1c0ad8ec521882ab0e4372758711c2d85
ElficTitious <ismael.correa.25@gmail.com>
2023-11-22 16:54:59 -0300

    refactor(InvoiceTable): added case when there is no data

--- src/components/InvoiceTable.js
+++ src/components/InvoiceTable.js
@@ -27,19 +27,27 @@ function SimpleTable({
     <div className="flex flex-col items-center space-y-4">
       <h2 className="text-xl font-medium">{header}</h2>
       <div className="w-3/5">
-        {tableData.map((row, index) => (
-          <TableRow
-            key={row.id}
-            row={row}
-            selected={
-              multiple ? selectedRows.includes(row.id) : selectedRows === row.id
-            }
-            onRowClick={() => handleRadioClick(row.id)}
-            usdToClpExchange={usdToClpExchange}
-            isUpperRow={index === 0}
-            isLowerRow={index === tableData.length - 1}
-          />
-        ))}
+        {tableData.length === 0 ? (
+          <div className="p-4 flex justify-center border rounded-lg">
+            <div>No hay datos</div>
+          </div>
+        ) : (
+          tableData.map((row, index) => (
+            <TableRow
+              key={row.id}
+              row={row}
+              selected={
+                multiple
+                  ? selectedRows.includes(row.id)
+                  : selectedRows === row.id
+              }
+              onRowClick={() => handleRadioClick(row.id)}
+              usdToClpExchange={usdToClpExchange}
+              isUpperRow={index === 0}
+              isLowerRow={index === tableData.length - 1}
+            />
+          ))
+        )}
       </div>
     </div>
   );
